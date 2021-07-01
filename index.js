f3ec6665c663374cab6b96e828aa4622aa7105ca
LeeJones95 <leonna.jones18@gmail.com>
2021-06-30 20:13:58 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(record){
+    // console.log(record)
+    const foundSeason = record.find(function(season){
+        return season.result === "W"
+    })
+    if(foundSeason){
+    return foundSeason.year
+    }
+}
+//if found season exists then we will return found season ^
